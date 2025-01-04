ae4c77ee1e5dcd618b925be0f953f6f316ce18d2
Max Shaughnessy <mshaugh@hey.com>
2025-01-04 09:18:20 +0000

    feat: update `nfwf.sh`

--- nfwf.sh
+++ nfwf.sh
@@ -1,173 +1,251 @@
 #!/bin/sh
 
-_contains() {
-	[ "${1#*$2}" != "$1" ] && return 0 || return 1
+if [ -n "${DEBUG+1}" ]; then
+	set -x
+fi
+
+set -a
+
+BASE_DIR="$(dirname "$(readlink -f "$0")")"
+BUILD_DIR="$(readlink -f "$BASE_DIR/build")"
+FONTS_DIR="$(readlink -f "$BASE_DIR/nerd-fonts")"
+
+set_action() {
+	if [ -n "${action+x}" ] && [ "$action" != "$1" ]; then
+		printf "Running %s with %s...\n" "$1" "$action"
+		printf "Incompatible options given. Only one action may be specified per run.\n"
+		exit 1
+	else
+		action="$1"
+	fi
 }
 
-_get_stretch() {
-	{ _contains "$1" "UltraCondensed" || _contains "$1" "Ultra Condensed" ;} &&
+contains() {
+	[ "${1#*"$2"}" != "$1" ] && return 0 || return 1
+}
+
+get_stretch() {
+	{ contains "$1" "UltraCondensed" || contains "$1" "Ultra Condensed"; } &&
 		printf "ultra-condensed\\n" && return 0
 
-	{ _contains "$1" "ExtraCondensed" || _contains "$1" "Extra Condensed" ;} &&
+	{ contains "$1" "ExtraCondensed" || contains "$1" "Extra Condensed"; } &&
 		printf "extra-condensed\\n" && return 0
 
-	{ _contains "$1" "SemiCondensed" || _contains "$1" "Semi Condensed" ;} &&
+	{ contains "$1" "SemiCondensed" || contains "$1" "Semi Condensed"; } &&
 		printf "semi-condensed\\n" && return 0
 
-	_contains "$1" "Condensed" && printf "condensed\\n" && return 0
+	contains "$1" "Condensed" && printf "condensed\\n" && return 0
 
-	{ _contains "$1" "SemiExpanded" || _contains "$1" "Semi Expanded" ;} &&
+	{ contains "$1" "SemiExpanded" || contains "$1" "Semi Expanded"; } &&
 		printf "semi-expanded\\n" && return 0
 
-	{ _contains "$1" "ExtraExpanded" || _contains "$1" "Extra Expanded" ;} &&
+	{ contains "$1" "ExtraExpanded" || contains "$1" "Extra Expanded"; } &&
 		printf "extra-expanded\\n" && return 0
 
-	{ _contains "$1" "UltraExpanded" || _contains "$1" "Ultra Expanded" ;} &&
+	{ contains "$1" "UltraExpanded" || contains "$1" "Ultra Expanded"; } &&
 		printf "ultra-expanded\\n" && return 0
 
-	_contains "$1" "Expanded" && printf "expanded\\n" && return 0
+	contains "$1" "Expanded" && printf "expanded\\n" && return 0
 
 	printf "normal\\n" && return 0
 }
 
-_get_style() {
-	_contains "$1" "Italic" && printf "italic\\n" && return 0
+get_style() {
+	contains "$1" "Italic" && printf "italic\\n" && return 0
 
-	_contains "$1" "Oblique" && printf "oblique\\n" && return 0
+	contains "$1" "Oblique" && printf "oblique\\n" && return 0
 
 	printf "normal\\n" && return 0
 }
 
-_get_weight() {
-	{ _contains "$1" "Thin" || _contains "$1" "Hairline" ;} &&
+get_weight() {
+	{ contains "$1" "Thin" || contains "$1" "Hairline"; } &&
 		printf "100\\n" && return 0
 
-	{ _contains "$1" "ExtraLight" || _contains "$1" "Extra Light" ||
-	  _contains "$1" "UltraLight" || _contains "$1" "Ultra Light" ;} &&
+	{ contains "$1" "ExtraLight" || contains "$1" "Extra Light" || contains "$1" "UltraLight" || contains "$1" "Ultra Light"; } &&
 		printf "200\\n" && return 0
 
-	_contains "$1" "Light" && printf "300\\n" && return 0
+	contains "$1" "Light" && printf "300\\n" && return 0
 
-	_contains "$1" "Medium" && printf "500\\n" && return 0
+	contains "$1" "Medium" && printf "500\\n" && return 0
 
-	{ _contains "$1" "SemiBold" || _contains "$1" "Semi Bold" ||
-	  _contains "$1" "DemiBold" || _contains "$1" "Demi Bold" ;} &&
+	{ contains "$1" "SemiBold" || contains "$1" "Semi Bold" || contains "$1" "DemiBold" || contains "$1" "Demi Bold"; } &&
 		printf "600\\n" && return 0
 
-	{ _contains "$1" "ExtraBold" || _contains "$1" "Extra Bold" ||
-	  _contains "$1" "UltraBold" || _contains "$1" "Ultra Bold" ;} &&
+	contains "$1" "Bold" && printf "700\\n" && return 0
+
+	{ contains "$1" "ExtraBold" || contains "$1" "Extra Bold" || contains "$1" "UltraBold" || contains "$1" "Ultra Bold"; } &&
 		printf "800\\n" && return 0
 
-	_contains "$1" "Bold" && printf "700\\n" && return 0
-
-	{ _contains "$1" "Black" || _contains "$1" "Heavy" ;} &&
+	{ contains "$1" "Black" || contains "$1" Heavy; } &&
 		printf "900\\n" && return 0
 
 	printf "400\\n" && return 0
 }
 
-_main() {
-	if [ -z "$TOKEN" ]
-	then
-		fonts=$(curl -Ls https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets | map(.browser_download_url) | @sh')
+upgrade() {
+	if [ ! -d "$FONTS_DIR" ]; then
+		mkdir "$FONTS_DIR"
+	fi
+	cd "$FONTS_DIR" || exit 1
+
+	if [ -z "$gh_token" ]; then
+		fonts_data=$(curl -Ls https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest)
 	else
-		fonts=$(curl -H "Authorization: token $TOKEN" -Ls https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest | jq -r '.assets | map(.browser_download_url) | @sh')
+		fonts_data=$(curl -H "Authorization: token $gh_token" -Ls https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest)
 	fi
 
-	for f in $fonts
-	do
-		url=$(echo $f | tr -d "'")
-		name=${url##*/}
-		base=${name%.*}
+	nf_fonts=$(printf "%s" "$fonts_data" | jq -r '.assets | map(select(.name | contains(".tar.xz"))) | map(.browser_download_url) | @sh')
+	fonts_urls=$(printf "%s" "$nf_fonts" | tr -d "'" | sed 's/ / -: -LO /g')
+	# shellcheck disable=SC2086
+	curl -LO $fonts_urls
 
-		[ -n "$UPGRADE" ] && curl -LO "$url"
+	cd "-" >/dev/null || exit 1
+}
 
-		if [ -n "$EXTRACT" ]
-		then
-			aunpack "$name"
-			find "$name" -type f -iname "*Windows Compatible*" -delete
-			find "$name" -type f -iname "*.otf" -delete
-		fi
+extract() {
+	cd "$FONTS_DIR" || exit 1
 
-		if [ -n "$CONVERT" ] || [ -n "$GENERATE" ]
-		then
-			cd "$base"
-			for file in ./*.ttf
-			do
-				if [ -f "$file" ]
-				then
-					if [ -n "$CONVERT" ]
-					then
-						printf "Converting $file...\\n"
-						fontforge -quiet -lang=ff -c "Open(\$1); Generate(\$1:r + \".woff2\")" "$file" 2>/dev/null
-					fi
+	count=$(find . -maxdepth 1 -type f | wc -l)
+	i=0
+	for file in ./*.tar.xz; do
+		i=$((i + 1))
+		basename=$(printf "%s" "$file" | sed 's/.tar.xz//g')
+		[ ! -d "$basename" ] && mkdir "$basename"
+		printf "[%d/%d] %s...\n" "$i" "$count" "$basename"
+		# shellcheck disable=SC2046
+		tar -x -f "$file" -C "$basename" $(tar -t -f "$file" | grep '.*\.[ot]tf')
+	done
 
-					if [ -n "$GENERATE" ]
-					then
-						printf "Generating CSS for ${file%.*}...\\n"
-						basen="$(basename "$file")"
-						family="$(fontforge -lang=ff -c "Open(\$1); Print(\$familyname);" "$file" 2>/dev/null)"
-						stretch="$(_get_stretch "$file")"
-						style="$(_get_style "$file")"
-						weight="$(_get_weight "$file")"
+	cd "-" >/dev/null || exit 1
+}
 
-						awk_script="$(readlink -f "$BASEDIR/scripts/$(printf "$base" | tr '[:upper:] ' '[:lower:]-').awk")"
-						[ -f "$awk_script" ] && builder="awk -f $awk_script" || builder="cat"
-						css="$($builder << EOF
+convert() {
+	cd "$FONTS_DIR" || exit 1
+
+	font_files=$(find . -depth 2 -type f | sort)
+	# shellcheck disable=SC2086
+	parallel --progress "fontforge -lang=ff -c 'Open(\$1); Generate(\$1:r + \".woff2\");' {} 2>/dev/null" ::: $font_files
+
+	cd "-" >/dev/null || exit 1
+}
+
+generate() {
+	cd "$FONTS_DIR" || exit 1
+
+	font_files=$(find . -depth 2 -type f | sort)
+	count=$(printf "%s" "$font_files" | wc -l)
+	i=0
+	for file in $font_files; do
+		i=$((i + 1))
+		printf "[%d/%d] %s...\n" "$i" "$count" "$file"
+		basen="$(basename "$file")"
+		font="$(basename "$(dirname "$file")")"
+		family="$(fontforge -lang=ff -c "Open(\$1); Print(\$familyname);" "$file" 2>/dev/null)"
+		stretch="$(get_stretch "$file")"
+		style="$(get_style "$file")"
+		weight="$(get_weight "$file")"
+
+		css="$(
+			cat <<EOF
 @font-face {
     font-family: "$family";
-    src:    local(${basen%.*}),
-            url("fonts/${basen%.*}.woff2") format("woff2"),
-            url("fonts/$basen") format("truetype");
+    src:    local("${basen%.*}"),
+            url("fonts/${basen%.*}.woff2") format("woff2");
     font-stretch: $stretch;
     font-style: $style;
     font-weight: $weight;
 }
 EOF
-)"
-						printf "$css\\n" >> "$BUILDDIR/nerdfont-webfonts.css"
-						printf "$css\\n" >> "$BUILDDIR/$(printf "$family" | tr '[:upper:] ' '[:lower:]-').css"
-					fi
-				fi
-			done
-			cd "$FONTDIR"
-		fi
+		)"
+
+		printf "%s\\n" "$css" >>"$BUILD_DIR/nerdfont-webfonts.css"
+		printf "%s\\n" "$css" >>"$BUILD_DIR/$(printf "%s" "$font" | tr '[:upper:] ' '[:lower:]-').css"
+		printf "%s\\n" "$css" >>"$BUILD_DIR/$(printf "%s" "$family" | tr '[:upper:] ' '[:lower:]-').css"
+	done
+
+	cd "-" >/dev/null || exit 1
+}
+
+docfonts() {
+	cd "$FONTS_DIR" || exit 1
+
+	cat >"$BASE_DIR/fonts.md" <<EOF
+# Fonts
+
+| Superfamily | CSS File |
+| ----------- | -------- |
+EOF
+
+	font_dirs=$(find . -depth 1 -type d | sort)
+	count=$(printf "%s" "$font_dirs" | wc -l)
+	i=0
+	for dir in $font_dirs; do
+		i=$((i + 1))
+		printf "[%d/%d] %s...\n" "$i" "$count" "$dir"
+		font="$(basename "$dir")"
+		file="$(printf "%s" "$font" | tr '[:upper:] ' '[:lower:]-').css"
+
+		md="$(
+			cat <<EOF
+| $font | [\`$file\`](./build/$file) |
+EOF
+		)"
+
+		printf "%s\\n" "$md" >>"$BASE_DIR/fonts.md"
 	done
 }
 
-while getopts 'acef:ght:u' o
-do
+info() {
+	cat <<EOF
+nfwf: Nerd Font Web Fonts
+
+Actions:
+  -c    Convert local nerd fonts to woff2
+  -d    Generate fonts.md
+  -e    Extract local archives
+  -g    Generate font face at-rules
+  -u    Fetch latest nerd fonts from upstream
+
+Options:
+  -f    Fonts directory
+  -t    GitHub API Token
+
+Dependencies:
+  curl          $(which curl)
+  fontforge     $(which fontforge)
+  jq            $(which jq)
+  parallel      $(which parallel)
+  tar           $(which tar)
+EOF
+}
+
+while getopts "cdeght:u" o; do
 	case "$o" in
-		a) CONVERT=1; EXTRACT=1; GENERATE=1; UPGRADE=1 ;;
-		c) CONVERT=1 ;;
-		e) EXTRACT=1 ;;
-		f) FONTDIR="$(readlink -f "$OPTARG")" ;;
-		g) GENERATE=1 ;;
-		t) TOKEN="$OPTARG" ;;
-		u) UPGRADE=1 ;;
+	c) set_action convert ;;
+	d) set_action docfonts ;;
+	e) set_action extract ;;
+	g) set_action generate ;;
+	h) set_action info ;;
+	t) gh_token="$OPTARG" ;;
+	u) set_action upgrade ;;
+	\?)
+		printf "See \`%s -h\` for possible options and help.\n" "$(basename "$0")"
+		exit 1
+		;;
 	esac
 done
 
+[ -z "$action" ] && action="info"
 
-if [ -d "$FONTDIR" ]
-then
-	BASEDIR="$(dirname "$(readlink -f "$0")")"
-	BUILDDIR="$(readlink -f "$BASEDIR/build")"
-	cd "$FONTDIR"
-	_main
-else
-	cat << EOF
-nfwf: Nerd Font Web Fonts
-
-Flags:
-  -a	Enable all actions
-  -c	Enable convert action
-  -e	Enable extract action
-  -f	Fonts directory
-  -g	Enable generate action
-  -h	Display this help menu
-  -t	GitHub API Token
-  -u	Enable upgrade action
-EOF
+case "$action" in
+convert) convert ;;
+docfonts) docfonts ;;
+extract) extract ;;
+generate) generate ;;
+upgrade) upgrade ;;
+info)
+	info
 	exit 1
-fi
+	;;
+esac
